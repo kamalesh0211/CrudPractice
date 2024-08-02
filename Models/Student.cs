70d0717642e25948c9e36639a75702e6ea0b4686
kamalesh0211 <kamaleshvivekanadan@gmail.com>
2024-08-03 00:38:35 +0530

    initial

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudPractice.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StudentName {  get; set; }
+        public string Course { get; set; }
+
+    }
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudPractice.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB; Initial Catalog=lbs; User Id=SS;TrustServerCertificate= True");
+        }
+    }
+}
